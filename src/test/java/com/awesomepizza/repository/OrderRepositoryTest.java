23ffb67f76e17439751855258ea1abd716ae4da1
Gaberiele <ciardo.gabriele@gmail.com>
2025-02-04 10:21:52 +0100

    clean imports

--- src/test/java/com/awesomepizza/repository/OrderRepositoryTest.java
+++ src/test/java/com/awesomepizza/repository/OrderRepositoryTest.java
@@ -1,17 +1,17 @@
 package com.awesomepizza.repository;
 
-import com.awesomepizza.model.Order;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import com.awesomepizza.model.Order;
 
 @DataJpaTest
 public class OrderRepositoryTest {
